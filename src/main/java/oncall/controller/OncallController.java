4ee6edb577b4aa2cdf86113f73502608b551b482
sinminseok12 <sinminseok12@naver.com>
2024-12-12 20:10:18 +0900

    feat(InputView) : 평일 근무자 이름 입력받는 기능 구현

--- src/main/java/oncall/controller/OncallController.java
+++ src/main/java/oncall/controller/OncallController.java
@@ -3,9 +3,12 @@ package oncall.controller;
 import oncall.dto.WorkDateRequest;
 import oncall.view.InputView;
 
+import java.util.List;
+
 public class OncallController {
 
     public void run(){
         WorkDateRequest workDateRequest = InputView.inputWorkDate();
+        List<String> weekdaysUsers = InputView.inputWeekdaysUser();
     }
 }
--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -7,6 +7,7 @@ import oncall.global.utils.Converter;
 import java.util.List;
 
 import static oncall.global.constants.ViewMessage.INPUT_MONTH_AND_DAY_MESSAGE;
+import static oncall.global.constants.ViewMessage.INPUT_WEEK_DAYS_USER_NAME;
 
 public class InputView {
     private InputView(){
@@ -19,5 +20,11 @@ public class InputView {
         List<String> splits = Converter.splitDelimiter(input);
         int month = Converter.toNumber(splits.get(0));
         return new WorkDateRequest(month, splits.get(1));
+    }
+
+    public static List<String> inputWeekdaysUser(){
+        System.out.println(INPUT_WEEK_DAYS_USER_NAME);
+        String input = Console.readLine();
+        return Converter.splitDelimiter(input);
     }
 }
